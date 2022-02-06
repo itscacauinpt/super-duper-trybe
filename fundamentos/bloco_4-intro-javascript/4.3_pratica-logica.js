b34ee095f018c3bb7d7041e938997e2528ed4cdf
cacau <anaclaudia.sza@outlook.com>
2022-02-06 00:09:51 -0300

    Resolvendo pratica logica -3

--- fundamentos/bloco_4-intro-javascript/4.3_pratica-logica.js
+++ fundamentos/bloco_4-intro-javascript/4.3_pratica-logica.js
@@ -10,4 +10,18 @@ for (let i = 1; i < 10 && i > 0; i+=1) {
 }
 console.log(fatorial);
 
+/**
+ * 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+*/
+let word = 'tryber';
+let reversed = '';
+
+for (let i = 0; i < word.length; i+=1){
+    //reversed += word.length-1;
+    //reversed += word.charAt(-1);
+    reversed += word.charAt(word.length-1-i);
+} 
+ console.log(reversed);
+
 
