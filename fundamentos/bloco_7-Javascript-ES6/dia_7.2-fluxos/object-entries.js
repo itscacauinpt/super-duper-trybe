a6f2afcdfcc9675db8fe26a99e887977f4e738ad
cacau <anaclaudia.sza@outlook.com>
2022-03-04 18:55:52 -0300

    Adiciona exercicios object-entries

---
+++ fundamentos/bloco_7-Javascript-ES6/dia_7.2-fluxos/object-entries.js
@@ -0,0 +1,27 @@
+//Object.entries, este retorna um array cujos elementos são também arrays para cada conjunto chave e valor do objeto.
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+console.log(Object.entries(coolestTvShow));
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.table(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
