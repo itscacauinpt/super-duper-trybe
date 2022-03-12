2d3c1438e84f60fada6d9edb0e5dceb517677aa4
cacau <anaclaudia.sza@outlook.com>
2022-03-12 12:38:57 -0300

    Dir 8.2

---
+++ fundamentos/bloco_8-higher-order-functions/dia_8.2/some-every.js
@@ -0,0 +1,30 @@
+//1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, 
+//retorne false , use some ;
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+  //return names.some((nomeEscolhido) => nomeEscolhido === name);
+  return arr.some((nomeEscolhido) => nomeEscolhido === name);
+}
+//console.log(hasName(names, 'Ana'))
+
+//2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual 
+//a mínima e caso contrário false , use every ;
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 18 },//era 18
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  //Adicione seu código aqui
+  return arr.every((theAge) => theAge.age >= minimumAge);
+  
+};
+
+console.log(verifyAges(people, 18));
