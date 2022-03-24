962f127d65f913bc5cdb6792920ae15dbfd15fe6
cacau <anaclaudia.sza@outlook.com>
2022-03-24 11:07:20 -0300

    nao tem nada aqui

---
+++ fundamentos/bloco_9-js-test-assincrono/dia_9.3/asyn-await-test.js
@@ -0,0 +1,3 @@
+// Você nunca deve testar uma requisição real para a API!
+// https://codesandbox.io/s/mentoria-180-busca-de-cep-e1keew?file=/src/script.js
+// mentoria para colar no projeto heheuehue
