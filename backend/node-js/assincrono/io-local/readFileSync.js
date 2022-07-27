3cf10f7f752e835215610ab71a2bf9689eed662c
cacau <anaclaudia.sza@outlook.com>
2022-07-27 20:06:47 -0300

    feat: fluxo assincrono, readFile, readFileSync e readFilePromise

---
+++ backend/node-js/assincrono/io-local/readFileSync.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const nomeDoArquivo = 'meu-arquivo.txt';
+
+try {
+  const data = fs.readFileSync(nomeDoArquivo, 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(`Erro ao ler o arquivo: ${err.path}`);
+  console.log(err);
+}
+
+// Se tivéssemos que ler vários arquivos ao mesmo tempo?
+// Resposta: Para isso, utilizamos um método assíncrono.
