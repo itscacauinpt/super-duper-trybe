75cd4ea57f7c6894722c7a269703bfbe8489ed74
cacau <anaclaudia.sza@outlook.com>
2022-08-01 21:59:01 -0300

    feat: fixando estruturacao de api

--- backend/node-js/express/estruturando-api/index.js
+++ backend/node-js/express/estruturando-api/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3005;
+
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
   { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
@@ -12,8 +14,21 @@ app.get('/recipes', function (req, res) {
   res.json(recipes);
 });
 
-app.listen(3001, () => {
-  console.log('Aplicação ouvindo na porta 3001');
+app.post('/recipes', function (request, response, next){
+  const { id, name } = request.body;
+  if (!id || id === '' || !name || name === '') return response.status(400).json({ message: 'input inválido' });
+
+  next();
+},
+function (request, response) {
+  const { id, name, price } = request.body;
+
+  recipes.push({ id, name, price });
+  response.status(201).json({ message: 'Receita adicionada com sucesso.' });
+});
+
+app.listen(PORT, () => {
+  console.log('Aplicação ouvindo na porta 3005');
 });
 
 // O método .send é um método que consegue retornar a resposta de uma requisição de uma forma genérica, 
