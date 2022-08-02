48acac2ec4e2e9654a29a35d72b32ea8da0f4947
cacau <anaclaudia.sza@outlook.com>
2022-08-01 23:02:02 -0300

    fix: app.get

--- backend/node-js/express/estruturando-api/index.js
+++ backend/node-js/express/estruturando-api/index.js
@@ -10,23 +10,36 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+// rest api (?) disponibilizar
 app.get('/recipes', function (req, res) {
-  res.json(recipes);
+  res.status(200).json(recipes);
 });
 
-app.post('/recipes', function (request, response, next){
-  const { id, name } = request.body;
-  if (!id || id === '' || !name || name === '') return response.status(400).json({ message: 'input inválido' });
+// app.get('/recipes/:id', function (request, response) {
+//   const { id } = request.params;
+//   const theRecipe = recipes.find((rec) => rec.id === Number(id));
 
-  next();
-},
-function (request, response) {
-  const { id, name, price } = request.body;
+//   if (!theRecipe) return recipes.status(404).json({ message: 'Error 404: recipe not found' });
 
-  recipes.push({ id, name, price });
-  response.status(201).json({ message: 'Receita adicionada com sucesso.' });
-});
+//   response.status(200).json(theRecipe);
+// });
 
+// postar - adicionar data
+// middleware de verificação de etrada de dados
+// app.post('/recipes', function (request, response, next){
+//   const { id, name } = request.body;
+//   if (!id || id === '' || !name || name === '') return response.status(400).json({ message: 'input inválido' });
+
+//   next();
+// },
+// function (request, response) {
+//   const { id, name, price } = request.body;
+
+//   recipes.push({ id, name, price });
+//   response.status(201).json({ message: 'Receita adicionada com sucesso.' });
+// });
+
+// rest (?)
 app.listen(PORT, () => {
   console.log('Aplicação ouvindo na porta 3005');
 });
