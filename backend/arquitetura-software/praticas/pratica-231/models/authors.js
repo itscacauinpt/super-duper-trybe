29cbf75ca3e9ef865936170c759f9165fd9a9e55
cacau <anaclaudia.sza@outlook.com>
2022-08-05 23:33:08 -0300

    fix: refatorando funcao de formatacao de dados

--- backend/arquitetura-software/praticas/pratica-231/models/authors.js
+++ backend/arquitetura-software/praticas/pratica-231/models/authors.js
@@ -1,6 +1,6 @@
 const connection = require('./connection');
 
-const addNewKey = ({ id, firstName, middleName, lastName }) => {
+const addFullName = ({ id, firstName, middleName, lastName }) => {
   const full = [firstName, middleName, lastName].filter((notEmpty) => notEmpty).join(' ');
   return {
     id, 
@@ -11,28 +11,25 @@ const addNewKey = ({ id, firstName, middleName, lastName }) => {
   }
 }
 
-const serialize = (authorData) => {
-  return {
-    id: authorData.id,
-    firstName: authorData.first_name,
-    middleName: authorData.middle_name,
-    lastName: authorData.last_name,
+const formated = (all) => {
+  const newFormate =  {
+    id: all.id,
+    firstName: all.first_name,
+    middleName: all.middle_name,
+    lastName: all.last_name,
   }
+  return addFullName(newFormate);
 }
 
 async function getAll() {
   const [ authors ] = await connection.execute('SELECT id, first_name, middle_name, last_name FROM model_example.authors');
-  return authors
-    .map(serialize)
-    .map(addNewKey);
+  return authors.map(formated);
 }
 
 async function getOne(id) {
   const query = 'SELECT id, first_name, middle_name, last_name FROM model_example.authors where id=?;';
   const [ author ] = await connection.execute(query, [id])
-  return author
-  .map(serialize)
-  .map(addNewKey);
+  return author.map(formated);
 }
 
 // adicionando novas pessoas
