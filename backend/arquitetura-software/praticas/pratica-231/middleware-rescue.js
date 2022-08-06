2b4fc8b93568793760b216ca37c2c15fe2266bdd
cacau <anaclaudia.sza@outlook.com>
2022-08-05 22:41:21 -0300

    feat: funcao para verificar novos autores

--- backend/arquitetura-software/praticas/pratica-231/middleware-rescue.js
+++ backend/arquitetura-software/praticas/pratica-231/middleware-rescue.js
@@ -13,7 +13,26 @@ async function verify(req, res, next) {
   next();
 }
 
+const isValid = (firstName, middleName, lastName) => {
+	if (!firstName || typeof firstName !== 'string') return false;
+	if (!lastName || typeof lastName !== 'string') return false;
+	if (middleName && typeof middleName !== 'string') return false;
+
+	return true;
+};
+
+async function verifyAuthor(req, res, next) {
+  const { first_name, middle_name, last_name } = req.body;
+
+	if (!isValid(first_name, middle_name, last_name)) {
+		return res.status(400).json({ message: 'Dados inválidos' });
+	}
+
+  next();
+}
+
 module.exports = {
   rescue,
   verify,
+  verifyAuthor,
 };
