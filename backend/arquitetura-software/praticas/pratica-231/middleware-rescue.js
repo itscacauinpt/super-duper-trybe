f5d3068a49d0dbd5895509945c24878406f7f238
cacau <anaclaudia.sza@outlook.com>
2022-08-05 22:21:13 -0300

    fix: dir duplicados

--- backend/arquitetura-software/praticas/pratica-231/middleware-rescue.js
+++ backend/arquitetura-software/praticas/pratica-231/middleware-rescue.js
@@ -6,4 +6,9 @@ const rescue = action => async (request, response, next) => {
   }
 }
 
+// async function rescue(_req, res, next) {
+
+//   if (!author) return res.status(404).json({ message: 'Not found' });
+// }
+
 module.exports = rescue;
