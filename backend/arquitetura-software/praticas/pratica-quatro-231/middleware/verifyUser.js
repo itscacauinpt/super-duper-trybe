e46c3cbdc6a6fb42360051932f4cc2907100b625
cacau <anaclaudia.sza@outlook.com>
2022-08-08 12:13:38 -0300

    feat: ativiade pratica 4 - dia 231

---
+++ backend/arquitetura-software/praticas/pratica-quatro-231/middleware/verifyUser.js
@@ -0,0 +1,8 @@
+// async function verifyUser(req, res, next) {
+//   const {  }
+//   // if (id !== ) return res.status(404).json({ message: 'User not found' });
+
+//   next();
+// }
+
+// module.exports = verifyUser;
