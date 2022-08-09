84e7824b8c8e9566b78b5612b4425a4b20fbdf52
cacau <anaclaudia.sza@outlook.com>
2022-08-08 23:14:24 -0300

    feat: rescue function

--- backend/arquitetura-software/controller-services-example/server.js
+++ backend/arquitetura-software/controller-services-example/server.js
@@ -5,12 +5,21 @@ const port = 3000;
 
 const Authors = require('./controller/authorsController');
 const Books = require('./controller/booksController');
+const rescue = require('./utils/rescue');
 
 app.use(bodyParser.json());
 
-app.get('/authors', Authors.getAll);
-app.get('/books', Books.getAll);
-app.get('/authors/:id', Authors.getById);
-app.get('/books/:id', Books.getById);
+app.get('/authors', rescue(Authors.getAll));
+app.get('/books', rescue(Books.getAll));
+app.get('/authors/:id', rescue(Authors.getById));
+app.get('/books/:id', rescue(Books.getById));
+
+app.post('/authors', rescue());
+
+app.use((err, _req, res, _next) => {
+  console.log(err);
+
+  return res.status(500).json({ code: 500, message: 'Erro no servidor!'});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
