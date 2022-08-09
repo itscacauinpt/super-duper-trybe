080337961ab98e84af5ba6157b28340d5d8c302d
cacau <anaclaudia.sza@outlook.com>
2022-08-08 23:02:47 -0300

    feat: adiciona controller getById books e authors

--- backend/arquitetura-software/controller-services-example/controller/booksController.js
+++ backend/arquitetura-software/controller-services-example/controller/booksController.js
@@ -6,6 +6,14 @@ async function getAll(_req, res) {
   return res.status(200).json(books)
 }
 
+async function getById(req, res) {
+  const { id } = req.params;
+  const book = await service.getByIdService(id);
+
+  return res.status(200).json(book);
+}
+
 module.exports = {
   getAll,
+  getById,
 }
