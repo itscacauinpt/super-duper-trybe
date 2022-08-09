e2bc15777d90fb696ddd737b88a61e452f9bb82f
cacau <anaclaudia.sza@outlook.com>
2022-08-08 23:02:17 -0300

    feat: services  getById books e authors

--- backend/arquitetura-software/controller-services-example/service/authorsService.js
+++ backend/arquitetura-software/controller-services-example/service/authorsService.js
@@ -1,10 +1,19 @@
 const model = require('../model/authorModel');
-// service onde as validações
+const format = require('../utils/formated');
+
 async function getAllService() {
   const authors = await model.GetAllModel();
   return authors;
 }
 
+async function getByIdService(id) {
+  const author = await model.getByIdModel(id);
+
+  if (!author[0]) return null;
+  return author.map(format.formatAuthors)[0];
+}
+
 module.exports = {
   getAllService,
+  getByIdService,
 }
