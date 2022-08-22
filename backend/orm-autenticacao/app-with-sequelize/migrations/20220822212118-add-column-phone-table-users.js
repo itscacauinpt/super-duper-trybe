45f25434bc1470694ecc205f4c7f55d52358df0d
cacau <anaclaudia.sza@outlook.com>
2022-08-22 18:31:43 -0300

    feat: add phone_num column into Users

--- backend/orm-autenticacao/app-with-sequelize/migrations/20220822212118-add-column-phone-table-users.js
+++ backend/orm-autenticacao/app-with-sequelize/migrations/20220822212118-add-column-phone-table-users.js
@@ -8,6 +8,9 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.addColumn('Users', 'phone_num', {
+      type: Sequelize.STRING,
+    });
   },
 
   async down (queryInterface, Sequelize) {
@@ -17,5 +20,7 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+     await queryInterface.removeColumn('Users', 'phone_num');
   }
 };
+// npx sequelize db:migrate
