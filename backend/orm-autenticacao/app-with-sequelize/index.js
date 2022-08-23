607f625a0d789fe54eb57f4fdb02cb86178f12b5
cacau <anaclaudia.sza@outlook.com>
2022-08-22 21:25:49 -0300

    feat: rotas e boas praticas

---
+++ backend/orm-autenticacao/app-with-sequelize/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+const userController = require('./controller/user');
+
+app.get('/user', userController);
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
