a2e8831e07028ca77f04a59cb7ff71ef4d12c8f1
cacau <anaclaudia.sza@outlook.com>
2022-08-24 22:19:44 -0300

    feat: associação de um para muitos

--- backend/orm-autenticacao/sequelize-associations/index.js
+++ backend/orm-autenticacao/sequelize-associations/index.js
@@ -18,6 +18,41 @@ app.get('/employees', async (_req, res) => {
   };
 });
 
+// http://localhost:3000/employees/1
+// ?includeAddresses=true
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    // eager loading:
+    // const employee = await Employee.findOne({
+    //     where: { id },
+    //     
+    //     // include: [{ model: Address, as: 'addresses' }],
+    //     include: [{model: Address, as: 'addresses', attributes: { exclude: ['employeeId', 'number'] },}]
+    //   });
+
+    // lazy loading:
+    const employee = await Employee.findOne({ where: { id } });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    // query string includeAddresses
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await Address.findAll({
+        where: { employeeId: id },
+        attributes: { exclude: ['employeeId', 'number'] } });
+
+      return res.status(200).json({ employee, addresses });
+    }
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
 
