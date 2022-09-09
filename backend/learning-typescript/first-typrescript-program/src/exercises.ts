67dfebdc2944f40777194d3aaf90cdd36dd27c98
cacau <anaclaudia.sza@outlook.com>
2022-09-09 00:09:06 -0300

    feat: adiciona novas para praticar

--- backend/learning-typescript/first-typrescript-program/src/exercises.ts
+++ backend/learning-typescript/first-typrescript-program/src/exercises.ts
@@ -30,3 +30,17 @@ export function calcQuad(side: number): number {
 export function calcRect(base: number, height: number): number {
   return base * height;
 }
+
+//
+// Crie uma nova função para calcular a área de um losango. A área do losango é dada pelo resultado da multiplicação da diagonal maior (D) pela diagonal menor (d) dividido por dois. (D * d) / 2
+// Calcule a área de um losango que tem D = 32cm e d = 18cm;
+// Calcule a área de um losango que tem D = 200cm e d = 50cm;
+// Calcule a área de um losango que tem D = 75cm e d = 25cm.
+// Crie uma nova função para calcular a área de um trapézio. A área do trapézio é dada pelo produto da altura (h) com a soma da base maior (B) e a base menor (b) dividido por dois. ((B + b) * h) / 2
+// Calcule a área de um trapézio que tem B = 100cm, b = 70cm e altura = 50cm;
+// Calcule a área de um trapézio que tem B = 75cm, b = 50cm e altura = 35cm;
+// Calcule a área de um trapézio que tem B = 150cm, b = 120cm e altura = 80cm.
+// Crie uma nova função para calcular a área de um círculo. A área do círculo de raio r é encontrada multiplicando o raio ao quadrado pelo número irracional ℼ (em geral utilizamos ℼ = 3,14). ℼ * r²
+// Calcule a área de um círculo de raio igual 25cm;
+// Calcule a área de um círculo de raio igual 100cm;
+// Calcule a área de um círculo de raio igual 12,5cm.
