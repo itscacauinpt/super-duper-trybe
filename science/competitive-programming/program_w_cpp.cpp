84694c511da5a10bef1d150e0d10f520cb533739
cacau <anaclaudia.sza@outlook.com>
2023-01-23 09:44:09 -0300

    feat: competitive programming with cpp

---
+++ science/competitive-programming/program_w_cpp.cpp
@@ -0,0 +1,20 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // the standard streams are cin for input and cout for output.
+  int a = 123, b = 321;
+  string x = "abc";
+  // Print output
+  // std::cout << "Hello World" << std::endl;
+  // cout << "Hello World" << endl;
+  cout << a << " " << b << " " << x << endl;
+
+  return 0;
+}
+
+
+// Take input using cin
+// cin >> a >> b >> x;
