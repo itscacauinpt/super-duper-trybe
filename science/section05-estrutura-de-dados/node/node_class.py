17389ba887da45ef9da8ff5976f23299ff24c510
cacau <anaclaudia.sza@outlook.com>
2023-02-01 19:36:08 -0300

    feat: data structure, nodes

---
+++ science/section05-estrutura-de-dados/node/node_class.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value):
+        self.value = value  # 🎲 Dado a ser armazenado
+        self.next = None  # 👉 Forma de apontar para outro nó
+
+    def __str__(self):
+        return f"Node(value={self.value}, next={self.next})"
+
+
+# node1 = Node('node um')
+# print(node1)
