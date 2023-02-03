ba8057631092a542c802db7a6133ee6572912739
cacau <anaclaudia.sza@outlook.com>
2023-02-03 15:54:56 -0300

    feat: add quick sort example

--- science/section04-algorithms/sorting/bubble_sort.py
+++ science/section04-algorithms/sorting/bubble_sort.py
@@ -1,18 +1,21 @@
 def bubble_sort(array):
-    swaps = 0
-    has_swapped = True
+    # swaps = 0
+    # has_swapped = True
 
-    while has_swapped:
-        has_swapped = False
+    for ordered_elements in range(len(array) - 1):
+        # while has_swapped:
+        # has_swapped = False
 
-        for index in range(len(array) - swaps - 1):
+        # for index in range(len(array) - swaps - 1):
+        for index in range(len(array) - ordered_elements - 1):
             if array[index] > array[index + 1]:
                 array[index], array[index + 1] = array[index + 1], array[index]
-                has_swapped = True
+                # has_swapped = True
 
-        swaps += 1
+        # swaps += 1
 
     return array
 
 
-# print(bubble_sort([10, 2, 4, 0, 1, 124]))
+# complexidade quadrática
+print(bubble_sort([10, 2, 4, 0, 1, 124]))
